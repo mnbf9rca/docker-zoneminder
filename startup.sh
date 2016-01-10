95c6978abbba5a6af7ed1011a68f17d9ef3f4f6e
mnbf9rca <robert.aleck@cynexia.com>
2016-01-10 09:42:23 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,24 +8,39 @@
   if [ "$(ls -A /config/zm)" ]; then
     echo "/config/zm not empty; creating symlink and updating schema"
     if [ "$(ls -A /var/lib/mysql/zm)" ]; then
-     echo "moving old db /zm"
+     echo "...found existing folder at /var/lib/mysql/zm"
+     echo "......stopping mysql"
      service mysql stop
+     echo "......moving old db /zm"
      mv --force /var/lib/mysql/zm /var/lib/mysql/zm-old
     fi
+    echo "...setting permissions"
     chmod --recursive --silent go+rw /config
+    echo "...creating symlink"
     ln -s /config/zm/ /var/lib/mysql/
+    echo "...setting owner"
     chown --recursive --silent mysql:mysql /var/lib/mysql
+    echo "...starting mysql"
     service mysql start
+    echo "...setting permissions"
     mysql -u root -e "grant select,insert,update,delete,create,alter,index,lock tables on zm.* to 'zmuser'@localhost identified by 'zmpass';"
+    echo "...updating schema"
     /usr/bin/zmupdate.pl
   else
     echo "/config/zm not found or empty; creating symlink and creating DB"
+    echo "...creating /config/zm"
     mkdir --parents /config/zm
+    echo "...setting permissions"
     chmod -R go+rw /config
+    echo "...creating symlink"
     ln -s /config/zm/ /var/lib/mysql/
+    echo "...changing owner"
     chown -R mysql:mysql /var/lib/mysql  
+    echo "...starting mysql"
     service mysql start
+    echo "...creating db"
     mysql < /usr/share/zoneminder/db/zm_create.sql
+    echo "...setting permissions"
     mysql -u root -e "grant select,insert,update,delete,create,alter,index,lock tables on zm.* to 'zmuser'@localhost identified by 'zmpass';"
   fi
 
@@ -40,5 +55,6 @@
   
   echo "starting other services"
   service apache2 start
-  service zoneminder start
+  
+  exit 0
 
